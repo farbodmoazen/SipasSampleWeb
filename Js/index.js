670275c71b5b923803a5bd873e76e014de58724e
siavashasachi <asachiciavash@gmail.com>
2024-08-29 13:13:23 +0330

    Add mobile menu

--- Js/index.js
+++ Js/index.js
@@ -53,3 +53,21 @@ function scrollFunction() {
 scrollTopBtn.addEventListener("click", function() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
 });
+
+
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.getElementById('menu-toggle');
+    const menu = document.getElementById('menu');
+
+    menuToggle.addEventListener('click', function() {
+        menu.classList.toggle('active');
+    });
+
+    // Optional: close the menu when clicking outside
+    document.addEventListener('click', function(event) {
+        if (!menu.contains(event.target) && !menuToggle.contains(event.target)) {
+            menu.classList.remove('active');
+        }
+    });
+});
